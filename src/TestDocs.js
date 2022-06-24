156b7c658120868c1eeec0a1388d942566eb45ed
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2022-06-24 10:49:30 -0400

    Filled out test docs page

--- src/TestDocs.js
+++ src/TestDocs.js
@@ -3,18 +3,18 @@ import "./App.css";
 function Sample() {
 
   return (
-    <div className=" min-h-screen">
+    <div className="bg-slate-200 min-h-screen">
       <header className="bg-white shadow-lg bg-slate-100">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 ">
           <h1 className="text-3xl font-bold text-gray-900 ">OMMC 2022 Test Documents</h1>
         </div>
       </header>
       <main className="bg-slate-200">
-      <div className="max-w-7xl mx-auto py-3 pt-7 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto py-3 pt-7 pb-7 sm:px-6 lg:px-8">
           <div className="px-4 sm:px-0">
             <div className="border-4 border-dashed border-gray-400 rounded-lg h-96 shadow-lg ">
                 <iframe
-                  src="https://drive.google.com/file/d/1p3RhXHcfPYCvm0oAAYodsUujbMPVwOR8/preview"
+                  src="https://drive.google.com/file/d/1eE5VRCuH5qBoGlfqo1zarOPA0kiFSbsx/preview"
                   class="w-full h-full"
                   allow="autoplay"
                 ></iframe>{" "}
@@ -22,30 +22,19 @@ function Sample() {
             </div>
           </div>
         </div>
-        <div className="max-w-7xl mx-auto py-4 pb-7 sm:px-6 lg:px-8">
-          <div className="px-4 sm:px-0">
-            <div className="border-4 border-dashed border-gray-400 rounded-lg h-96 shadow-lg ">
-                <iframe
-                  src="https://drive.google.com/file/d/1VWXcQy5waWyRwuG8J64LxkJUETCPTvC4/preview"
-                  class="w-full h-full"
-                  allow="autoplay"
-                ></iframe>{" "}
-                
-            </div>
-          </div>
-        </div>
+        
       </main>
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900 ">Solutions</h1>
+          <h1 className="text-3xl font-bold text-gray-900 ">Tiebreaker</h1>
         </div>
       </header>
       <main className="rows-2 bg-slate-200">
-        <div className="max-w-7xl mx-auto py-3 pt-7 sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto py-3 pt-7 pb-7  sm:px-6 lg:px-8">
           <div className="px-4 sm:px-0">
             <div className="border-4 border-dashed border-gray-400 rounded-lg h-96 shadow-lg ">
                 <iframe
-                  src="https://drive.google.com/file/d/1CZKocvVETJW7p0_bQLMWfkWCo8kEuJYr/preview"
+                  src="https://drive.google.com/file/d/18gzfx_VjCUSvsEFsA8GsHUSwj2e4Gk1q/preview"
                   class="w-full h-full"
                   allow="autoplay"
                 ></iframe>{" "}
@@ -53,18 +42,7 @@ function Sample() {
             </div>
           </div>
         </div>
-        <div className="max-w-7xl mx-auto py-4 pb-7 sm:px-6 lg:px-8">
-          <div className="px-4 sm:px-0">
-            <div className=" border-4 border-dashed border-gray-400 rounded-lg h-96 shadow-lg ">
-                <iframe
-                  src="https://drive.google.com/file/d/1NDweAK-Os3m9M4b3Pp2Mf_x7wDUTjPsr/preview"
-                  class="w-full h-full"
-                  allow="autoplay"
-                ></iframe>{" "}
-                
-            </div>
-          </div>
-        </div>
+        
       </main>
     </div>
   );
