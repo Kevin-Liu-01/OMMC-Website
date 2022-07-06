6845e55a74d2a540e2b609b55a71250bcdd708d5
Kevin-Liu-01 <66856750+Kevin-Liu-01@users.noreply.github.com>
2022-07-06 11:04:06 -0400

    Added solutions to test docs

--- src/Pages/TestDocs.js
+++ src/Pages/TestDocs.js
@@ -46,6 +46,28 @@ function TestDocs() {
         </div>
         
       </main>
+
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900 ">Solutions</h1>
+        </div>
+      </header>
+      <main className="rows-2 bg-slate-200">
+        <div className="max-w-7xl mx-auto py-3 pt-7 pb-7  sm:px-6 lg:px-8">
+          <div className="px-4 sm:px-0">
+            <div className="border-4 border-dashed border-gray-400 rounded-lg h-96 shadow-lg ">
+                <iframe
+                  title="solutions"
+                  src="https://drive.google.com/file/d/1XqDo3fqmGLzZyswu7ohd0z7DJxBnDa5z/preview"
+                  class="w-full h-full"
+                  allow="autoplay"
+                ></iframe>{" "}
+                
+            </div>
+          </div>
+        </div>
+        
+      </main>
     </div>
   );
 }
