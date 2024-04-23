13800b8b32c3fd20db8608e693509e8065fd86ef
pranshu suyal <suyalpranshu@gmail.com>
2024-04-23 11:00:11 -0400

    updated banner so link opens in new tab

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -41,6 +41,7 @@ export default function Banner() {
             </a> */}
             <a
               href="https://artofproblemsolving.com/community/c5h3306582_cash__raffle_prizes_ommc_year_4"
+              target="_blank"
               className="hover:bg-red-100 dark:hover:bg-[#921717] transform duration-150 ease-in-out  flex items-center 
               justify-center px-4 py-2 rounded-lg shadow-sm text-sm font-medium text-red-800 border-transparent dark:text-white bg-white dark:bg-red-800 select-none "
             >
