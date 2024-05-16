7751de48c352c6a301822697f09e1b9284895ffa
Kevin Liu <kk23907751@gmail.com>
2024-05-15 21:59:40 -0400

    [Feat] Return test portal button

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -32,13 +32,13 @@ export default function Banner() {
             </p>
           </div>
           <div className="order-3 grid mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto sm:flex sm:flex-row">
-            {/* <a
+            <a
               href="https://ommc-test-portal.vercel.app/"
               className="hover:bg-red-100 dark:hover:bg-[#921717] transform duration-150 ease-in-out  flex items-center 
               justify-center px-3 mr-2 py-2 rounded-lg shadow-sm text-sm font-medium text-red-800 border-transparent dark:text-white bg-white dark:bg-red-800 select-none"
             >
               <ExternalLinkIcon className="h-4 w-4 mr-1" /> Test Portal
-            </a> */}
+            </a>
             <a
               href="https://artofproblemsolving.com/community/c5h3306582_cash__raffle_prizes_ommc_year_4"
               target="_blank"
