79a9a15d9e14679c710bc0a79beb17dd8772553b
Pranshu Suyal <suyalpranshu@gmail.com>
2025-05-15 21:20:00 -0400

    updated learn more link

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -38,7 +38,7 @@ export default function Banner() {
               <ExternalLinkIcon className="h-4 w-4 mr-1" /> Test Portal
             </a>
             <a
-              href="https://artofproblemsolving.com/community/c5h3306582_cash__raffle_prizes_ommc_year_4"
+              href="https://artofproblemsolving.com/community/c5h3556611_main_round_starts_may_17_ommc_year_5"
               className="hover:bg-red-100 dark:hover:bg-[#921717] transform duration-150 ease-in-out  flex items-center 
               justify-center px-4 py-2 rounded-lg shadow-sm text-sm font-medium text-red-800 border-transparent dark:text-white bg-white dark:bg-red-800 select-none "
             >
