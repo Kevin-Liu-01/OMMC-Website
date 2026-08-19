057e5224ae3f250ac4a52b90cb584ffb9bbb47b7
DottedCalculator <112578558+DottedCalculator@users.noreply.github.com>
2026-08-19 16:57:54 -0400

    Update Banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -25,8 +25,8 @@ export default function Banner() {
               />
             </span>
             <p className="ml-3 font-medium text-white truncate">
-              <span className="md:hidden">OMMC Year 5 has passed!</span>
-              <span className="hidden md:inline">OMMC Year 5 has passed!</span>
+              <span className="md:hidden">OMMC Year 6!</span>
+              <span className="hidden md:inline">The OMMC Year 6 Main Round will occur on September 12-19!</span>
             </p>
           </div>
           <div className="order-3 gap-2 grid mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto sm:flex sm:flex-row">
@@ -38,7 +38,7 @@ export default function Banner() {
               <ExternalLinkIcon className="h-4 w-4 mr-1" /> Test Portal
             </a>
             <a
-              href="https://artofproblemsolving.com/community/c5h3556611_main_round_starts_may_17_ommc_year_5"
+              href="https://artofproblemsolving.com/community/c5h3888246s2_ommc_year_6_september_1219"
               className="hover:bg-red-100 dark:hover:bg-[#921717] transform duration-150 ease-in-out  flex items-center 
               justify-center px-4 py-2 rounded-lg shadow-sm text-sm font-medium text-red-800 border-transparent dark:text-white bg-white dark:bg-red-800 select-none "
             >
