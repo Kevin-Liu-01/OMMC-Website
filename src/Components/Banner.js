b721451a5bf3a0ddc08d6ad58872bec564737b1f
DottedCalculator <112578558+DottedCalculator@users.noreply.github.com>
2026-08-19 17:03:34 -0400

    Update Banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -31,11 +31,11 @@ export default function Banner() {
           </div>
           <div className="order-3 gap-2 grid mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto sm:flex sm:flex-row">
             <a
-              href="https://ommc-test-portal-2025.vercel.app/"
+              href=""
               className="hover:bg-red-100 dark:hover:bg-[#921717] transform duration-150 ease-in-out  flex items-center 
               justify-center px-3 py-2 rounded-lg shadow-sm text-sm font-medium text-red-800 border-transparent dark:text-white bg-white dark:bg-red-800 select-none"
             >
-              <ExternalLinkIcon className="h-4 w-4 mr-1" /> Test Portal
+              <ExternalLinkIcon className="h-4 w-4 mr-1" /> Test Portal Coming Soon
             </a>
             <a
               href="https://artofproblemsolving.com/community/c5h3888246s2_ommc_year_6_september_1219"
