ab6edd346c805fa3a647386850355ef9216f4a5f
ericguo01 <137231985+ericguo01@users.noreply.github.com>
2026-09-12 12:17:18 -0400

    Update Banner.js

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -26,7 +26,7 @@ export default function Banner() {
             </span>
             <p className="ml-3 font-medium text-white truncate">
               <span className="md:hidden">OMMC Year 6!</span>
-              <span className="hidden md:inline">The OMMC Year 6 Main Round will occur on September 12-19!</span>
+              <span className="hidden md:inline">The OMMC Year 6 Main Round is happening now! You have until September 19 to submit!</span>
             </p>
           </div>
           <div className="order-3 gap-2 grid mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto sm:flex sm:flex-row">
@@ -35,7 +35,7 @@ export default function Banner() {
               className="hover:bg-red-100 dark:hover:bg-[#921717] transform duration-150 ease-in-out  flex items-center 
               justify-center px-3 py-2 rounded-lg shadow-sm text-sm font-medium text-red-800 border-transparent dark:text-white bg-white dark:bg-red-800 select-none"
             >
-              <ExternalLinkIcon className="h-4 w-4 mr-1" /> Test Portal Coming Soon
+              <ExternalLinkIcon className="h-4 w-4 mr-1" /> Test Portal
             </a>
             <a
               href="https://artofproblemsolving.com/community/c5h3888246s2_ommc_year_6_september_1219"
